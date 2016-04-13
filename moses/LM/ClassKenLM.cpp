5e7384393df263802e284fa637ca8595178b4546
W. Ferreira Aziz <wferrei1@laco10.science.uva.nl>
2016-04-13 16:17:24 +0200

    lowercase-keys switch in ClassKenLM

--- moses/LM/ClassKenLM.cpp
+++ moses/LM/ClassKenLM.cpp
@@ -15,7 +15,7 @@ namespace Moses
 namespace
 {
 
-void LoadWordToClassMapping(const std::string& path, std::map<std::string, std::string>& mapping)
+void LoadWordToClassMapping(const std::string& path, std::map<std::string, std::string>& mapping, const bool lowercase_keys)
 {
     mapping.clear();
     std::ifstream fin(path.c_str());
@@ -25,7 +25,10 @@ void LoadWordToClassMapping(const std::string& path, std::map<std::string, std::
         if (tokens.empty()) 
             continue;
         UTIL_THROW_IF2(tokens.size() != 2, "Expected exactly 2 tokens: <word> <class>");
-        mapping[tokens[0]] = tokens[1];
+        if (lowercase_keys)
+            mapping[ToLower(tokens[0])] = tokens[1];
+        else
+            mapping[tokens[0]] = tokens[1];
     }
 }
 
@@ -36,10 +39,12 @@ template <class Model> ClassKenLM<Model>::ClassKenLM(const std::string &line,
         const std::string &file, 
         FactorType factorType, 
         bool lazy, 
-        const std::string& classPath)
-  :LanguageModelKen<Model>(line, file, factorType, lazy)
+        const std::string& classPath,
+        const bool lowercase_keys)
+  :LanguageModelKen<Model>(line, file, factorType, lazy),
+    m_lowercase_keys(lowercase_keys)
 {
-    LoadWordToClassMapping(classPath, m_word_to_class);
+    LoadWordToClassMapping(classPath, m_word_to_class, lowercase_keys);
 }
 
 //template <class Model> ClassKenLM<Model>::ClassKenLM(const ClassKenLM<Model> &copy_from)
@@ -52,29 +57,30 @@ LanguageModel* ConstructClassKenLM(const std::string& line,
         const std::string& file, 
         FactorType factorType, 
         bool lazy, 
-        const std::string& classPath)
+        const std::string& classPath,
+        const bool lowercase_keys)
 {
   lm::ngram::ModelType model_type;
   if (lm::ngram::RecognizeBinary(file.c_str(), model_type)) {
 
     switch(model_type) {
     case lm::ngram::PROBING:
-      return new ClassKenLM<lm::ngram::ProbingModel>(line, file, factorType, lazy, classPath);
+      return new ClassKenLM<lm::ngram::ProbingModel>(line, file, factorType, lazy, classPath, lowercase_keys);
     case lm::ngram::REST_PROBING:
-      return new ClassKenLM<lm::ngram::RestProbingModel>(line, file, factorType, lazy, classPath);
+      return new ClassKenLM<lm::ngram::RestProbingModel>(line, file, factorType, lazy, classPath, lowercase_keys);
     case lm::ngram::TRIE:
-      return new ClassKenLM<lm::ngram::TrieModel>(line, file, factorType, lazy, classPath);
+      return new ClassKenLM<lm::ngram::TrieModel>(line, file, factorType, lazy, classPath, lowercase_keys);
     case lm::ngram::QUANT_TRIE:
-      return new ClassKenLM<lm::ngram::QuantTrieModel>(line, file, factorType, lazy, classPath);
+      return new ClassKenLM<lm::ngram::QuantTrieModel>(line, file, factorType, lazy, classPath, lowercase_keys);
     case lm::ngram::ARRAY_TRIE:
-      return new ClassKenLM<lm::ngram::ArrayTrieModel>(line, file, factorType, lazy, classPath);
+      return new ClassKenLM<lm::ngram::ArrayTrieModel>(line, file, factorType, lazy, classPath, lowercase_keys);
     case lm::ngram::QUANT_ARRAY_TRIE:
-      return new ClassKenLM<lm::ngram::QuantArrayTrieModel>(line, file, factorType, lazy, classPath);
+      return new ClassKenLM<lm::ngram::QuantArrayTrieModel>(line, file, factorType, lazy, classPath, lowercase_keys);
     default:
       UTIL_THROW2("Unrecognized kenlm model type " << model_type);
     }
   } else {
-    return new ClassKenLM<lm::ngram::ProbingModel>(line, file, factorType, lazy, classPath);
+    return new ClassKenLM<lm::ngram::ProbingModel>(line, file, factorType, lazy, classPath, lowercase_keys);
   }
 }
 
@@ -84,6 +90,7 @@ LanguageModel* ConstructClassKenLM(const std::string& line)
   std::string filePath;
   std::string classPath;
   bool lazy = false;
+  bool lowercase_keys = false;
 
   std::vector<std::string> toks = Tokenize(line);
   for (std::size_t i = 1; i < toks.size(); ++i) {
@@ -101,6 +108,9 @@ LanguageModel* ConstructClassKenLM(const std::string& line)
     } else if (args[0] == "mapping") {
       UTIL_THROW_IF2(!FileExists(args[1]), "Word-class mapping not found: " + args[1]);
       classPath = args[1];
+    } else if (args[0] == "lowercase-keys") {
+      if (args[1] == "true" || args[1] == "1" || args[1] == "yes")
+        lowercase_keys = true;
     } else if (args[0] == "name") {
       // that's ok. do nothing, passes onto LM constructor
     }
@@ -109,7 +119,7 @@ LanguageModel* ConstructClassKenLM(const std::string& line)
   UTIL_THROW_IF2(filePath.empty(), "ClassKenLM requires path=<path>");
   UTIL_THROW_IF2(classPath.empty(), "ClassKenLM requires word-to-class-mapping=<path>");
 
-  return ConstructClassKenLM(line, filePath, factorType, lazy, classPath);
+  return ConstructClassKenLM(line, filePath, factorType, lazy, classPath, lowercase_keys);
 }
 
 
--- moses/LM/ClassKenLM.h
+++ moses/LM/ClassKenLM.h
@@ -25,19 +25,30 @@ LanguageModel* ConstructClassKenLM(const std::string& line,
         const std::string& file, 
         FactorType factorType, 
         bool lazy, 
-        const std::string& classPath);
+        const std::string& classPath,
+        const bool lowercase_keys);
 
 
 template <class Model> class ClassKenLM : public LanguageModelKen<Model>
 {
 public:
-  ClassKenLM(const std::string &line, const std::string &file, FactorType factorType, bool lazy, const std::string& classPath);
+  ClassKenLM(const std::string &line, 
+          const std::string &file, 
+          FactorType factorType, 
+          bool lazy, 
+          const std::string& classPath,
+          const bool lowercase_keys);
 
 protected:
   std::map<std::string, std::string> m_word_to_class;
+  bool m_lowercase_keys;
 
   lm::WordIndex TranslateID(const Word &word) const {
-      std::map<std::string, std::string>::const_iterator it = m_word_to_class.find(SPrint(word.GetString(this->m_factorType)));
+      std::map<std::string, std::string>::const_iterator it;
+      if (!m_lowercase_keys)
+         it = m_word_to_class.find(SPrint(word.GetString(this->m_factorType)));
+      else
+         it = m_word_to_class.find(ToLower(SPrint(word.GetString(this->m_factorType))));
       if (it == m_word_to_class.end())
         return this->m_ngram->GetVocabulary().Index(word.GetString(this->m_factorType)); 
       return this->m_ngram->GetVocabulary().Index(it->second); 
